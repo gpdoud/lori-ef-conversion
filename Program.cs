b8f0e91cbbbf439578eae8636961792c0d603fbb
Greg Doud <gpdoud@gmail.com>
2023-12-14 19:47:19 -0500

    Added TableViewReader and Extensions

--- Program.cs
+++ Program.cs
@@ -1,43 +1,25 @@
-﻿using lori_ef_conversion;
+﻿using Microsoft.Data.SqlClient;
+
+using TableViewRepository;
 
 Console.WriteLine("Lori w/EF");
 
-var context = new AppDbContext();
+TableViewReader tvr = new TableViewReader {
+    Server = "ripper",
+    Instance = "sqlexpress",
+    Database = "LoriDb",
+    UserId = "dsi",
+    Password = "dsiadmin",
+    SQL = "SELECT * From Invoices order by FFname, InvoiceNbr;"
+};
 
-var fileLines = new Dictionary<string, List<string>>();
+tvr.ExecuteRead();
+var cols = new string[] {
+    "FFname", "Lines", "TotAmt", "CurDate", "InvoiceNbr",
+    "InvDate", "ItemNbr", "Quantity", "UnitPrice", "ItemSalesAmt"
+};
+var recordList = tvr.TextOutput(cols);
 
-var companyList = (from inv in context.Invoices
-                   orderby inv.FFname
-                   select new {
-                       inv.FFname
-                   }).Distinct();
-
-foreach(var company in companyList.ToList()) {
-
-    var invoiceList = (from inv in context.Invoices
-                       where inv.FFname == company.FFname
-                       orderby inv.InvoiceNbr
-                       select new {
-                           Name = inv.FFname, Invoice = inv.InvoiceNbr
-                       }).Distinct();
-
-    var lines = new List<string>();
-
-    foreach(var inv in invoiceList.ToList()) {
-
-        lines.Add($"H N:{inv.Name}, I:{inv.Invoice} ");
-        var total = 0m;
-        foreach(var item in context.Invoices.Where(x => x.InvoiceNbr == inv.Invoice).ToList()) {
-            lines.Add($"D I:{item.ItemNbr}, Q:{item.Quantity}, P:{item.UnitPrice:C}, LT:{item.Quantity * item.UnitPrice:C}");
-            total += item.Quantity * item.UnitPrice;
-        }
-        lines.Add($"S TOTAL:{total:C}\n");
-    }
-
-    fileLines.Add(company.FFname, lines);
-}
-
-foreach(var company in fileLines.Keys) {
-    var fullpath = Path.Combine(Directory.GetCurrentDirectory(), "../../..", $"{company}.txt");
-    File.WriteAllLines(fullpath, fileLines[company]);
+foreach(var record in recordList) {
+    Console.WriteLine($"{record}");
 }
